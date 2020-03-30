e84df193908f8cd23a9e51a72dc9780c3bb75778
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-30 09:33:26 +0300

    first views

--- olx/product/models.py
+++ olx/product/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     name=models.CharField(max_length=100)
     owner=models.ForeignKey(User, on_delete=models.CASCADE)
     category=models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    brand=models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
     description=models.TextField(max_length=1000)
     condition=models.CharField(max_length=100, choices=CONDITION_TYPE)
     price=models.DecimalField(max_digits=15, decimal_places=5)
@@ -28,3 +29,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+
+class Brand(models.Model):
+    brand_name=models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name='Brand'
+        verbose_name_plural='Brands'
+
+    def __str__(self):
+        return self.brand_name
+
+
+class ProductImages(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.product.name
--- olx/product/views.py
+++ olx/product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    product_list=Product.objects.all()
+    return render(request, 'product/product_list.html', {'product_list':product_list})
+
+def product_detail(request, id):
+    product=Product.objects.get(pk=id)
+    return render(request, 'product/product_detail<id>.html', {'product':product})
