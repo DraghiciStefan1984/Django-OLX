9c08d6747005f4bd289fffe07b064d7e6b9f0d0f
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-04-01 09:14:33 +0300

    pagination start

--- olx/product/views.py
+++ olx/product/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Product, ProductImages
+from django.core.paginator import Paginator
 
 # Create your views here.
 def product_list(request):
     product_list=Product.objects.all()
+    paginator = Paginator(product_list, 1)
+    page_number = request.GET.get('page')
+    product_list = paginator.get_page(page_number)
     return render(request, 'product/product_list.html', {'product_list':product_list})
 
 def product_detail(request, product_slug):
