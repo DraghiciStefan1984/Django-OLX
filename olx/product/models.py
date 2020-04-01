08acb7e984d41caf20e312690cfea08f2ed961da
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-04-01 07:42:53 +0300

    fixed images

--- olx/product/models.py
+++ olx/product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
@@ -13,10 +14,17 @@ class Product(models.Model):
     description=models.TextField(max_length=1000)
     condition=models.CharField(max_length=100, choices=CONDITION_TYPE)
     price=models.DecimalField(max_digits=15, decimal_places=5)
+    image=models.ImageField(upload_to='main_product/', blank=True, null=True)
     created=models.DateTimeField(default=timezone.now)
+    slug=models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug=slugify(self.name)
+            super().save(*args, **kwargs)
 
 
 class Category(models.Model):
--- olx/product/views.py
+++ olx/product/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, ProductImages
 
 # Create your views here.
 def product_list(request):
     product_list=Product.objects.all()
     return render(request, 'product/product_list.html', {'product_list':product_list})
 
-def product_detail(request, id):
-    product=Product.objects.get(pk=id)
-    return render(request, 'product/product_detail<id>.html', {'product':product})
+def product_detail(request, product_slug):
+    product_detail=Product.objects.get(slug=product_slug)
+    product_images=ProductImages.objects.filter(product=product_detail)
+    context={'product_detail':product_detail, 'product_images':product_images}
+    return render(request, 'product/product_detail.html', context)
