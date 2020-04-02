a734d21710447801c444757054ee8068f4f05ebe
draghicistefan <draghicistefan1984@gmail.com>
2020-04-02 10:33:44 +0300

    registration continued

--- olx/account/urls.py
+++ olx/account/urls.py
@@ -1,10 +1,16 @@
 
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login as auth_view
 from . import views
 
 app_name='account'
 
 urlpatterns = [
-    # path('login/', login,  {'template_name':'account.login.html'}, name='login')
+    # path('login/', auth_view.LoginView.as_view(), name='login'),
+    # path('password_change/', auth_view.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', auth_view.LoginView.as_view(), name='password_change_done'),
+    # path('password_reset/', auth_view.LoginView.as_view(), name='password_reset'),
+    # path('password_reset/done/', auth_view.LoginView.as_view(), name='password_reset_done'),
+    # path('password_reset/confirmed/<uid64>/<token>/', auth_view.LoginView.as_view(), name='password_reset_confirm'),
+    # path('password_reset/complete/', auth_view.LoginView.as_view(), name='password_reset_complete'),
 ]
--- olx/olx/urls.py
+++ olx/olx/urls.py
@@ -19,10 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('product.urls', namespace='products')),
-    # path('account/', include('account.urls', namespace='account')),
     path('account/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('products/', include('product.urls', namespace='products'))
+    # path('account/', include('account.urls', namespace='account')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
